21441b8ce869e463d44c3bc2ee86c359ca67e2d0
Al Davidson <apdavidson@gmail.com>
2014-12-17 16:41:56 +0000

    backend selection, better debug logging

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -3,26 +3,38 @@ require 'local_resource'
 
 class ScansController < ApplicationController
   def create
-    file = local_file
-    scanner = Services::VirusScanners.instance('ClamAv')
-    puts "scanning #{local_file}"
+    file = local_file(params)
+    scanner = Services::VirusScanners.instance(backend(params))
+    puts "scanning #{file}"
     safe = scanner.is_safe?(file)
     puts "=> #{safe}"
 
-    File.delete(file) if params[:file_url].present?
+    # if we were given a URL, then we will have downloaded a 
+    # local copy - so lets clean up after ourselves
+    if params[:file_url].present?
+      puts "deleting local file #{file}"
+      File.delete(file) 
+    end
+
     render json: safe, status: 200
   end
 
   private
 
-  def local_file
-    if params[:file_url].present?
-      puts "downloading local copy of #{params[:file_url]}"
-      file = Services::LocalResource.new(URI.parse(params[:file_url])).file.path
-    elsif params[:file].present?
-      file = params[:file]
+  def local_file(opts={})
+    if opts[:file_url].present?
+      puts "downloading local copy of #{opts[:file_url]}"
+      file = Services::LocalResource.new(URI.parse(opts[:file_url])).file.path
+    elsif opts[:file].present?
+      file = opts[:file]
     else
       raise 'No file or file_url parameter supplied!'
     end
+  end
+
+  def backend(opts={})
+    (Services::VirusScanners.available & 
+      Array(opts[:backend] || ENV['RAVE_DEFAULT_BACKEND'] || 'ClamAv')
+    ).first
   end
 end
--- app/services/virus_scanners.rb
+++ app/services/virus_scanners.rb
@@ -3,7 +3,13 @@ require 'virus_scanners/clam_av'
 require 'virus_scanners/meta_scan'
 
 module Services::VirusScanners
+
   def self.instance(name = nil, args = {})
+    raise ArgumentError.new('unknown backend') unless available.include?(name)
     "Services::VirusScanners::#{name.classify}".constantize.new(args)
+  end
+
+  def self.available
+    ['ClamAv', 'MetaScan']
   end
 end
